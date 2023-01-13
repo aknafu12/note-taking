e5071e91c6b669eabf6f437c7ab43a2a39376302
aknafu12 <kgebre2021@gmail.com>
2023-01-12 21:49:03 -0800

    first time commit

---
+++ REST_API/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    
+     return '%s %s' %(self.title, self.body)
