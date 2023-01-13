99fd9ce0b8533c55fcfbc17145b961814a2a6ed2
aknafu12 <kgebre2021@gmail.com>
2023-01-13 04:35:02 -0800

    wire up the URLs, VIEWS to diplay the data

---
+++ REST_API/urls.py
@@ -0,0 +1,16 @@
+#app urls
+from django.urls import path, include
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'python',views.NoteViewSet)
+
+# Wire up  API using automatic URL routing
+urlpatterns = [
+    path('',include(router.urls))
+    #to login
+    path('api-auth/',include('rest_framework.urls', namespace='rest_framework'))
+
+
+]
