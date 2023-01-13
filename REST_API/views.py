a5e406896731044abe0e0c4249677c9205979bc6
aknafu12 <kgebre2021@gmail.com>
2023-01-13 04:06:44 -0800

    view implemented

--- REST_API/views.py
+++ REST_API/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Note
+from .serializers import NoteSerializers
 
 # Create your views here.
+class NoteViewSet(viewsets.ModelViewSet):
+    queryset = Note.objects.all().order_by('title')
+    serializer_class = NoteSerializers
